cffbbed6b55638f316f35c8f3bfcd9a2dddb1aca
DasShorty <anthony.timmel@dasshorty.de>
2026-05-19 09:28:47 +0200

    prepare for part 3

---
+++ 03_PART_3/02_Software/templates/humidity/history-converter.js
@@ -0,0 +1,21 @@
+let history = global.get('humid-history') || [];
+
+let timeLabel = new Date().toLocaleTimeString('de-DE', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+});
+
+history.push({
+    time: timeLabel,
+    value: msg.payload,
+    timestamp: new Date()
+});
+
+let hourAgo = Date.now() - (60 * 60 * 1000);
+history = history.filter(record => record.timestamp >= hourAgo);
+
+global.set('humid-history', history);
+
+msg.payload = history;
+return msg;
